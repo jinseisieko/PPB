29845f842da6994933fc93f38652174096eac064
Andrchest <andreipolevoi220@gmail.com>
2024-04-13 15:58:10 +0300

    coin game

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 from discord import Embed, Color
 
 import log_functions
@@ -11,7 +13,7 @@ from config import config
 def main():
     intents = discord.Intents.default()
     intents.message_content = True
-    bot = commands.Bot(command_prefix='->', intents=intents)
+    bot = commands.Bot(command_prefix='.', intents=intents)
 
     @bot.event
     async def on_ready():
@@ -32,6 +34,33 @@ def main():
         answer = answer.content
         if answer == "aboba":
             await ctx.channel.send(':)')
+
+    @bot.command(name="coin")
+    async def heads_or_tails(ctx):
+        def check(m):
+            return m.author.id == ctx.author.id
+
+        while True:
+            tmp = Embed(title="Подбрасываю монетку!", colour=Color.from_rgb(0, 255, 0))
+            tmp.set_image(url=
+                          "https://cdn.discordapp.com/attachments/1227605957086019615/1228682002308399164/-50.gif?ex=662cee49&is=661a7949&hm=d69f16298361988d3840daf2f7e9349edcc33f6bd1d4d22b9eddfd9b9ce8833c&")
+
+            await ctx.send(embed=tmp)
+            await ctx.send(embed=Embed(title="Как думаешь, что выпало?", colour=Color.from_rgb(0, 255, 0)))
+
+            answer = await bot.wait_for('message', check=check)
+            answer = answer.content
+            dct = {0: "решка", 1: "орел"}
+            real_result = dct[random.randint(0, 1)]
+            if real_result in answer.lower():
+                await ctx.channel.send('ПОБЕДА ПОСЛЕ ОБЕДА!!!1!!1!!1!!1!!111!1!!')
+            else:
+                await ctx.channel.send('ЖЕЕЕЕЕСТЬ! ТАКОГО ЛОХА ЕЩЕ ПОИСКАТЬ НАДО! АХАХАХАХААХАХ!!!!!!11!!1!1!')
+            await ctx.channel.send('Хотите еще раз проигр... Попытать свою удачу, конечно?')
+            answer = await bot.wait_for('message', check=check)
+            answer = answer.content
+            if "нет" in answer.lower():
+                break
 
     bot.run(config['token'])
 
