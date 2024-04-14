fa62e9886d8081d1ca499e155ffe192675940d4a
Andrchest <andreipolevoi220@gmail.com>
2024-04-14 14:04:49 +0300

    coin game ready with fake points

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def main():
         await ctx.send('pong')
 
         def check(m):
-            return m.author.id == ctx.author.id
+            return m.author.id == ctx.author.id and m.channel.id == ctx.channel.id
 
         answer = await bot.wait_for('message', check=check)
         answer = answer.content
@@ -38,29 +38,58 @@ def main():
     @bot.command(name="coin")
     async def heads_or_tails(ctx):
         def check(m):
-            return m.author.id == ctx.author.id
+            return m.author.id == ctx.author.id and m.channel.id == ctx.channel.id
 
         while True:
-            tmp = Embed(title="Подбрасываю монетку!", colour=Color.from_rgb(0, 255, 0))
+            tmp = Embed(title="Подбрасываю монетку!", colour=Color.from_rgb(255, 215, 0))
             tmp.set_image(url=
                           "https://cdn.discordapp.com/attachments/1227605957086019615/1228682002308399164/-50.gif?ex=662cee49&is=661a7949&hm=d69f16298361988d3840daf2f7e9349edcc33f6bd1d4d22b9eddfd9b9ce8833c&")
 
             await ctx.send(embed=tmp)
-            await ctx.send(embed=Embed(title="Как думаешь, что выпало?", colour=Color.from_rgb(0, 255, 0)))
+            await ctx.send(embed=Embed(title="Как думаешь, что выпало?\n"
+                                             "1. Решка\n"
+                                             "2. Орел\n"
+                                             "Напиши цифру и свою ставку через пробел!",
+                                       colour=Color.from_rgb(255, 215, 0)))
 
+            while True:
+                try:
+                    answer = await bot.wait_for('message', check=check)
+                    answer = answer.content
+                    real_result = random.randint(1, 2)
+                    if real_result == int(answer.split()[0]):
+                        await ctx.channel.send(f'Действительно, ты прав!\n +{int(answer.split()[1])} points!')
+                    elif real_result != int(answer.split()[0]) and int(answer.split()[0]) in [1, 2]:
+                        await ctx.channel.send(f'Нет, ты не угадал!\n -{int(answer.split()[1])} points!')
+                    else:
+                        await ctx.channel.send('Я не понимаю. Введи только нужную цифру')
+                        continue
+                    break
+                except Exception:
+                    await ctx.channel.send('Я не понимаю. Введи только нужную цифру')
+                    continue
+
+            await ctx.channel.send(embed=Embed(title="Хочешь еще раз сыграть?\n"
+                                                     "1. Да\n", colour=Color.from_rgb(255, 215, 0)))
             answer = await bot.wait_for('message', check=check)
             answer = answer.content
-            dct = {0: "решка", 1: "орел"}
-            real_result = dct[random.randint(0, 1)]
-            if real_result in answer.lower():
-                await ctx.channel.send('ПОБЕДА ПОСЛЕ ОБЕДА!!!1!!1!!1!!1!!111!1!!')
-            else:
-                await ctx.channel.send('ЖЕЕЕЕЕСТЬ! ТАКОГО ЛОХА ЕЩЕ ПОИСКАТЬ НАДО! АХАХАХАХААХАХ!!!!!!11!!1!1!')
-            await ctx.channel.send('Хотите еще раз проигр... Попытать свою удачу, конечно?')
-            answer = await bot.wait_for('message', check=check)
-            answer = answer.content
-            if "нет" in answer.lower():
+            try:
+                if 1 == int(answer.split()[0]):
+                    continue
+            except Exception:
                 break
+            break
+
+    @bot.command(name="test")
+    async def test(ctx):
+        def check(m):
+            return m.author.id == ctx.author.id and m.channel.id == ctx.channel.id
+
+        tmp = Embed(title="test", colour=Color.from_rgb(255, 215, 0))
+        tmp.set_image(url=
+                      "https://cdn.discordapp.com/attachments/1178308857999654983/1229022138569195590/image.png?ex=662e2b0f&is=661bb60f&hm=3ef7638013c2b4cc80819b6bfe7bbed9535eacbb6e03cd8bcefc4bfaa08ef1ea&")
+
+        await ctx.send(embed=tmp)
 
     bot.run(config['token'])
 
