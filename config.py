c604eddec8c92cdc26e04f82634a2fa89ed1963c
jinseisieko <144102580+jinseisieko@users.noreply.github.com>
2024-04-14 20:04:37 +0300

    development

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 config = {
-    'token_1': 'MTIyNzU5MjU3MjIzNTQxNTU5Mg.GwHZrl.CFfnVkVhHpweL133vxEzRGugrxQNDy4uYSbqYo',
-    'token': 'MTIyODk5NTQ0OTcxNzkxNTY1OA.GCWoM-.W6nN2MRMWMcdBa-OuEqZ8-pFuMPYLOPfSO9EG0',
+    'token': 'MTIyNzU5MjU3MjIzNTQxNTU5Mg.GwHZrl.CFfnVkVhHpweL133vxEzRGugrxQNDy4uYSbqYo',
     'user_id': 1178287016404856847,
+
 }
